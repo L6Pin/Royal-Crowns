4dcbd537c768b40a6740dee7243b3c493df665c2
Luka Peric <lukaperickv@gmail.com>
2020-12-19 20:45:02 +0100

    commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,30 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from '../img/logo.png';
-import { Link } from 'react-router-dom';
 
-const Nav = () => (
+function Nav() {
 
-   <div className="nav">
+   const [isMenuopen, setIsMenuOpen] = useState(false);
 
-      <nav className="logo">
-      <img src={logo} className="logo__img" alt="Royal Crowns Logo"/>
-      {/* <p className="logo__lang">RS</p> */}
-      {/* <p className="logo__lang">EN</p> */}
-      <p className="logo__text">Royal <br/> Crowns</p>
-      </nav>
+   
+   function openMenu(){
+      setIsMenuOpen(!isMenuopen);
+      console.log(isMenuopen);
+   }
 
-      <div className="menuBtn">
-         <span className="menuLineBtn menuLineBtn--1"></span>
-         <span className="menuLineBtn menuLineBtn--2"></span>
-         <span className="menuLineBtn menuLineBtn--3"></span>
+   return (
+
+      <div className="nav">
+
+         <nav className="logo">
+            <img src={logo} className={isMenuopen ? "logo__img hide" : "logo__img"} alt="Royal Crowns Logo" />
+            <p className={isMenuopen ? "logo__lang" : "logo__lang hide"}>RS</p>
+            <p className={isMenuopen ? "logo__text hide" : "logo__text"}>Royal <br /> Crowns</p>
+         </nav>
+
+         <div className="menuBtn" onClick={openMenu}>
+            <span className={isMenuopen ? "menuLineBtn menuLineBtn--1" : "menuLineBtn"}></span>
+            <span className={isMenuopen ? "menuLineBtn menuLineBtn--2" : "menuLineBtn"}></span>
+            <span className={isMenuopen ? "menuLineBtn menuLineBtn--3" : "menuLineBtn"}></span>
+         </div>
+
+         <div className={isMenuopen ? "menu menu__open" : "menu"}>
+            <img src={logo} alt="Menu Background" className="menu__bg" />
+            <div className="menu__links">
+               <p>JOIN THE CLUB</p>
+               <p>AMERICAN FOOTBALL</p>
+               <p>ATHLETICS</p>
+               <p>CLUB #3</p>
+               <p>CLUB #4</p>
+               <p>FIND A MEMBER</p>
+            </div>
+         </div>
+
       </div>
 
-      <div className="menu">
-         <img src={logo} alt="Menu Background" className="menu__bg"/>
-      </div>
+   );
 
-   </div>
-
-);
+}
 
 export default Nav;
+
+
