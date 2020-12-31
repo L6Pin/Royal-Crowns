b79ed20ec8e5e1906debcb5def5ab2cd3881e8d9
Luka Peric <lukaperickv@gmail.com>
2020-12-31 10:31:07 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Form from './sections/form';
 import Teams from './sections/teams';
 import FoundMember from './sections/foundMember';
 import Clubs from './sections/clubs';
+import SingleClub from './sections/singleClub';
 
 
 function App() {
@@ -23,7 +24,8 @@ function App() {
         <Route path='/teams' component={Teams} />
         <Route path='/findMember' exact component={FindMember} />
         <Route path='/findMember/:id' component={FoundMember} />
-        <Route path='/clubs' component={Clubs} />
+        <Route path='/clubs' exact component={Clubs} />
+        <Route path='/clubs/:id' exact component={SingleClub} />
         </Switch>
 
     </Router>
