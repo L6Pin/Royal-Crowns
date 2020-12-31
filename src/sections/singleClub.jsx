388dda9d2a83dccff3bfa7694b3d510680b9d256
Luka Peric <lukaperickv@gmail.com>
2020-12-31 17:22:08 +0100

    commit

--- src/sections/singleClub.jsx
+++ src/sections/singleClub.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Teams from '../teamsData.json';
-
+import Members from '../data.json';
 
 export default function SingleClub(match) {
 
+    let br = 0;
 
 
     return (
+        
         Teams.map(team => {
 
             if (match.match.params.id === team.id) {
@@ -19,6 +21,7 @@ export default function SingleClub(match) {
                                 <h2>{team.team}</h2>
                                 <p>{team.teamSubtitle}</p>
                             </div>
+                            {console.log(match.match)}
                             <div className="posterGradient"></div>
                         </div>
                         <div className="clubInfoContainer">
@@ -27,8 +30,39 @@ export default function SingleClub(match) {
                             <img src={team.teamPicture} alt="" />
                             <p>{team.meetTheTeam2}</p>
 
-                            {/* Latest members */}
-                            {console.log(match.match.params.id)}
+                            {
+                            
+                                Members.map(member => {
+
+                                    
+                                    if(member.teamId === match.match.params.id){
+                                        br = br + 1;
+                                        if (br < 3){
+                                            return(
+                                                <div className="member">
+                                                <div className="member__imageContainer">
+                                                    <img src={member.img} alt="Member Profile Image" />
+                                                </div>
+                                                <div className="member__profileContainer">
+                                                    <span>Name:</span>
+                                                    <p>{member.name}</p>
+                                            
+                                                    <span>Team:</span>
+                                                    <p>{member.team}</p>
+                                            
+                                                    <span>Age:</span>
+                                                    <p>{member.age}</p>
+                                                </div>
+                                            </div>
+                                            )
+                                        }
+                                    }
+                                   
+
+                            
+
+                                })
+                            }
 
                             <h3>Ready to join?</h3>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quisquam magni ea veritatis, distinctio aliquid voluptatibus corporis est, impedit beatae exercitationem quos minus incidunt repellat quibusdam expedita explicabo facilis id?</p>
