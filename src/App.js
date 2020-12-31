7c8a9b02421c26efa87d28d861287334ef3bf6c4
Luka Peric <lukaperickv@gmail.com>
2020-12-31 17:51:01 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './css/style.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Nav from './components/nav.js';
 import Index from './sections/index';
